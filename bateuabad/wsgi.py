64f9ecfb510251641c0f6867c11087b47cbde466
Rodrigo Richter <guigo_richter@hotmail.com>
2016-07-05 21:30:22 -0300

    updated wsgi and requirements for heroku

--- bateuabad/wsgi.py
+++ bateuabad/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-"""default file configuration
+
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,10 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bateuabad.settings")
 
 application = get_wsgi_application()
-"""
 
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
